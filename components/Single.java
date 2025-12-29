1697ef911a6906046089356c72bfda6f3eca1bae
Carod <imran.danial2006@gmail.com>
2025-12-29 19:28:51 +0800

    "cantikkan lagi single"

--- components/Single.java
+++ components/Single.java
@@ -4,20 +4,23 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.border.LineBorder;
+import models.Gambar;
 
 public class Single extends JFrame {
 	public final static int width = 960 * 8 / 10;
 	public final static int height = 540 * 8 / 10;
 	public final static int gW = width * 3/10;
 	public final static int gH = gW * 6/4;
-	public Single(String src) { //get the url from img folder
+	public Single(Gambar item, String src) { //get the url from img folder
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		//this.setLayout(new GridLayout(1, 2));
@@ -40,23 +43,48 @@ public class Single extends JFrame {
 		//this.add(label);
 
 
-
+		//BINA PANEL DESCRIPTION====================================================================
 		JPanel panelDesc = new JPanel();
+		panelDesc.setLayout(new BorderLayout());
 
+
+		//ADE TITLE====================================================================
 		JLabel title = new JLabel("Pizza on a Cup");
 		title.setFont(new Font("Ariel", Font.BOLD, 27));
+
+
+		//Ade CATEGORY, HARGA, CART BUTTON=====================================================================
+		JPanel panelKiriDesc = new JPanel();
+		panelKiriDesc.setLayout(new GridLayout(3, 1));
+		panelKiriDesc.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		JLabel category = new JLabel(item.getCatagory());
+		JLabel harga = new JLabel("RM " + item.getPrice());
+		JButton addToCart = new JButton("Add To Cart");
+		//addToCart.setPreferredSize(new Dimension(80, 10));
+		panelKiriDesc.add(category);
+		panelKiriDesc.add(harga);
+		panelKiriDesc.add(addToCart);
+
+
+		//ADE DESCRIPTION PUNYE TEXT======================================================================
 		JTextArea desc = new JTextArea(
 			"You see, pizza usually put on a plate or a flat board. " +
 			"But this..this is masterpiece. This is truly out of this " + 
-			"world. No camparison."
+			"world. No camparison. Lets add more line to do some experiments"
 		);
-		desc.setPreferredSize(new Dimension(368, 200));
+		desc.setPreferredSize(new Dimension(368, 100));
+		desc.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		//desc.setBackground(Color.decode("#eeeeee"));
 		desc.setEditable(false);
 		desc.setFocusable(false); // optional
 		desc.setLineWrap(true);
 		desc.setWrapStyleWord(true);
-		panelDesc.add(title);
-		panelDesc.add(desc);
+
+
+		//LETAK SEMUA BENDA KAT PANEL DESCRIPTION======================================================================
+		panelDesc.add(title, BorderLayout.NORTH);
+		panelDesc.add(panelKiriDesc, BorderLayout.WEST);
+		panelDesc.add(desc, BorderLayout.SOUTH);
 		//this.add(label2);
 
 
